e7e7fca11d2f30485656bc41d5d4926bf38bcdd0
nogsposito <vsan@cesar.school>
2024-12-04 13:13:05 -0300

    modelos e etc criados

--- api_rest/models.py
+++ api_rest/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_nickname = models.CharField(max_length=100, primary_key=True, default="") #primaryKey = id. (nao poderao ter dois iguais)
+    user_name = models.CharField(max_length= 150, default="")
+    user_email = models.EmailField(default="")
+    user_age = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Nickname: {self.user_nickname} | E-mail: {self.user_email}"
+
+class UsserTasks(models.Model):
+    user_nickname = models.CharField(max_length=100, default="")
+    user_task = models.CharField(max_length=255, default="")
--- api_root/settings.py
+++ api_root/settings.py
@@ -129,6 +129,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ALLOW_ORIGINS = [
     'http://localhost:8080'
 ]
